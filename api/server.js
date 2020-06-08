6eed8e20edc111fb52e70943c4c484e8e2fa93d1
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-08 15:50:29 -0400

    Added a CRUD requests for get and get by id

--- api/server.js
+++ api/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const knex = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+const path = "/api/accounts";
+
+server.get(`${path}`, (req, res) => {
+    knex.select('*').from('accounts')
+    .then(accounts => {
+        res.status(200).json(accounts)
+    })
+})//working
+
+server.get(`${path}/:id`, (req, res) => {
+    knex.select('*').from('accounts').where({id: req.params.id})
+    .first()
+    .then(account => {
+        res.status(200).json(account)
+    })
+})
 
 module.exports = server;
