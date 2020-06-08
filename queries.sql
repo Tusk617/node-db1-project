6a9869c87c7316768730c0ef007a798e1ec4b2af
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-08 16:16:27 -0400

    Completed two stretch problems in queries.sql

--- queries.sql
+++ queries.sql
@@ -20,5 +20,6 @@ VALUES ('Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 UPDATE Customers SET CustomerName = "The Shire", PostalCode = "11122" WHERE CustomerID = "92"; 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-
+SELECT COUNT (DISTINCT City) FROM Suppliers;
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT * FROM Suppliers WHERE LENGTH("SupplierName") > 20;
